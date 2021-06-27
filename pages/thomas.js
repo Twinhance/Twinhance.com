a703b4ebc68b3e4186cd7a9e6474900d6dae1c32
Thomakiin <Thomakiin@gmail.com>
2021-06-27 10:26:28 -0400

    updated thomas page

--- pages/thomas.js
+++ pages/thomas.js
@@ -1,19 +1,29 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../comps/Layout'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/thomas.module.css'
+import Image from 'next/image'
+//import profilePic from '../public/TMProfilePic.jpg'
 
 export default function Home() {
     return (
         <>
-            <Layout>
-                <Head>
-                    <title>Thomas Morris</title>
-                </Head>
-                <h1 className={styles.title}>Thomas Morris</h1>
-                <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-                <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-            </Layout>
+            <Head>
+                <title>Thomas Morris</title>
+            </Head>
+
+           {/* <Image src={profilePic} alt="Picture of the author" />*/}
+            <img src="/TMProfilePic.jpg" alt="Picture of the author" />
+            <h1 className={styles.tm} >Thomas Morris</h1>
+            <p className={styles.tm}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+
+            <h1>Projects:</h1>
+
+            <div class="grid-container">
+                <div class="grid-item">Typing Game</div>
+                <div class="grid-item">Periwiggle</div>
+            </div>
         </>
     )
 }
