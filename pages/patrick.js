94cca16a3557c26b71a2b605e56788d2185a6cea
Patrick <patthenoble0@gmail.com>
2021-06-27 09:40:35 -0400

    Spruced up Patrick Page

--- pages/patrick.js
+++ pages/patrick.js
@@ -2,23 +2,60 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../comps/Layout'
 import Image from 'next/image'
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+// Open in new tab safely. This is why we do it safely: https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/
+const openInNewTab = (url) => {
+  const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+  if (newWindow) newWindow.opener = null
+}
 
 export default function Home() {
   return (
     <>
-    <div className="wallpaper">
-{/*         <Image src="/vercel.svg" sizes="100%" layout="fill" objectFit="fill" /> */}
-    </div>
-    <Layout>
-    <Head>
+    {/* <Image src="/vercel.svg" sizes="100%" layout="fill" objectFit="fill" />  */}
+    <html className="PM">
+      <Head>
         <title>About | Patrick</title>
         <meta name="keywords"/>
       </Head>
-      <div>
-        <h1>Patrick</h1>
-        <p>h1 guyz, th1z 1s m3</p>
+
+
+
+      <body className="PM">
+        <div className="PM content gradient">
+          <div className="PM profile"></div>
+          <h1 className="PM">Patrick Morris</h1>
+        </div>
+      </body>
+
+      <div className="PM grid-container">
+        <div className="PM grid-item" onClick={()=> openInNewTab("https://github.com/PatTheNoble")}>Github</div>
+        <div className="PM grid-item" onClick={()=> openInNewTab("https://twitter.com/twinhance")}>Game Dev</div>
+        <div className="PM grid-item" onClick={()=> openInNewTab("https://github.com/PatTheNoble")}>Web Dev</div>   
       </div>
-    </Layout>
+
+        
+      {/* {
+        React.createElement(
+          'span',
+          {className: 'mushroom'}
+        )
+      } */}
+      </html>
     </>
   )
 }
+/*
+if (typeof window === 'object') {
+   // Check if document is finally loaded
+   var i;
+   for (i = 0; i < 29; i++) {
+     let div = document.createElement('div');
+     div.classList.add('PM');
+     div.classList.add('mushroom');
+     document.body.appendChild(div)
+   }
+}
+*/
