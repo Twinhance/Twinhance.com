54a18d2c2cfa9d7de58c1574624fdc5f22e515e7
Patrick <patthenoble0@gmail.com>
2021-07-13 18:27:06 -0400

    Added responsive nav bar and moved personal page logic

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,77 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../comps/Layout'
-import styles from '../styles/Home.module.css'
+import Blog from '../comps/Blog'
+import Image from 'next/image'
+import { useState } from 'react'
 
-export default function Home() {
+//Props from getStaticProps()
+export default function Home(props) {
+  const [hamburgerClass, setHamburgerClass] = useState("");
+
+  function manageBugerClick(){
+    console.log("Hello");
+    if(hamburgerClass === "drop-down"){
+      setHamburgerClass("");
+    }
+    else{
+      setHamburgerClass("drop-down");
+    }
+  }
+
+  const blogs = props.files.map(file => (
+    <li key={file.fileName}>
+      <Blog
+        name={file.fileName}
+        link={file.link}
+        key={file.fileName}
+      />
+    </li>
+  ));
+
   return (
-    <>
-    <Layout>
-    <Head>
+    // <div className="Twinhance page" style={{backgroundImage:"url(Gradient-BG.svg)", backgroundRepeat:"no-repeat", backgroundSize:"100%"}}>
+    <div className="Twinhance page">
+      <Head>
         <title>Twinhance</title>
-        <meta name="keywords"/>
+        <meta name="keywords" />
+        <link rel="shortcut icon" href="/Twinhance-Logo2.svg" />
       </Head>
-      <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      </div>
-    </Layout>
-    </>
+      <nav>
+        <img src="https://PatTheNoble.github.io/Hamburger.svg" className={hamburgerClass + " hamburger"} alt="Hamburger" width="23px" onClick={manageBugerClick}/>
+        <a href="" className={hamburgerClass}>Home</a>
+        <a href="" className={hamburgerClass}>Home</a>
+        <a href="" className={hamburgerClass}>Home</a>
+      </nav>
+      <img className="shake" src="Twinhance-Logo2.svg" alt="Twinhance Logo" width="40%" style={{margin:"auto", display: "block"}}/>
+
+      <section className="content">
+          <div>
+            <h1>About:</h1>
+            <p>
+              We are groot. Lorem Ipsum delore Lorem Ipsum delore Lorem Ipsum delore Lorem Ipsum delore Lorem Ipsum delore Lorem Ipsum delore 
+            </p>
+
+            <h1>Blogs:</h1>
+            <ul>
+              {blogs}
+            </ul>
+          </div>
+      </section>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  var fs = require('fs');
+  var files = [];
+  var fileNames = fs.readdirSync('../twinhance/pages/blog');
+  files = fileNames.map(fileName => {
+    return {
+      fileName: fileName,
+      link: '/blog/' + fileName.substring(0, fileName.length - 3) // Substring to remove the .js at the end.
+    };
+  })
+  console.log(files);
+  return { props: { files: files } }
 }
